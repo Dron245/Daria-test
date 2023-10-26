44cdef463ddde3ea9a6015f5b9a1630bda8ae864
Dron245 <Dron245@yandex.ru>
2023-10-26 23:03:16 +0300

    2610-2303

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,30 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+	
+	document.addEventListener('click', actions)
+	function actions (e) {
+		
+		function sw() {
+			console.log(e.target.innerText);
+		const target= e.target
+		if(e.target.closest('.actions__language' && target.innerText === 'EN')){
+					e.target.innerText = "RU";
+					
+					console.log(e.target.innerText); 
+				}
+		if(e.target.closest('.actions__language') && target.innerText === 'RU'){
+			console.log('123');
+			e.target.innerText = "EN";
+			
+		};
+		}
+		sw()
+		
+	}
+		
+}
