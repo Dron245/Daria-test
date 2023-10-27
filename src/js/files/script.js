cc31e880f4d2d36f605e1fa64f236e84bbb7cd75
Dron245 <Dron245@yandex.ru>
2023-10-27 15:09:05 +0300

    2710-1509

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -7,25 +7,21 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
 	
-	document.addEventListener('click', actions)
-	function actions (e) {
-		
-		function sw() {
-			console.log(e.target.innerText);
-		const target= e.target
-		if(e.target.closest('.actions__language' && target.innerText === 'EN')){
-					e.target.innerText = "RU";
-					
-					console.log(e.target.innerText); 
-				}
-		if(e.target.closest('.actions__language') && target.innerText === 'RU'){
-			console.log('123');
-			e.target.innerText = "EN";
-			
+	window.addEventListener('click', (e)=>{
+		const target = e.target
+		const btnLang = document.querySelector('.actions__language')
+		if(e.target.closest('.actions__language')){
+			btnLang.classList.toggle('switch-lang')
 		};
+		// console.log(document.querySelector('.menu__icon'));
+		// console.log(e.target);
+		if (!e.target.closest('.menu__icon') && !target.closest('.menu__body')) { 
+			document.documentElement.classList.remove("menu-open");
+			document.documentElement.classList.remove("lock");
 		}
-		sw()
-		
-	}
-		
+	})
+	AOS.init({
+		// easing: 'ease-out-back',
+		duration: 500,
+	});
 }
